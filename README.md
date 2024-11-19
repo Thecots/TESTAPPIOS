0a7474c5abb10d0be372e10cfe7fd87b25a69a82
thecots cots <thecots22@gmail.com>
2024-11-19 18:35:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+·KEY WORDS: RUTINA, SERIES, PESO, REPETICIONES, 
+
+·QUE QUIERO: Una aplicación para poder llevar el progreso de mis rutinas.
+
+
+-> RUTINAS
+    Se podrán crear rutinas, las cuales es
---
+++ app.js
@@ -0,0 +1,23 @@
+class Routine{
+    id;
+    name;
+    date;
+    exercises;
+}
+
+class Exercise{
+    id;
+    name;
+    sets;
+}
+
+class Sets{
+    id;
+    right;
+    left;
+    Dropset
+}
+
+class Dropset{
+    
+}
